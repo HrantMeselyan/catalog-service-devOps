05094e021432e4a07d3172ab67673a6cc565a5b4
Hrant Meselyan <hrant3307@gmail.com>
2024-04-26 15:04:26 +0400

    initial commit

--- src/main/java/com/example/catalogservicedevops/controller/HomeController.java
+++ src/main/java/com/example/catalogservicedevops/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
     public String home() {
         return "Welcome to Catalog Service";
     }
