34e088545003b3d24daa74f7471b241220d0c586
Hrant Meselyan <hrant3307@gmail.com>
2024-04-26 15:00:44 +0400

    initial commit

---
+++ src/main/java/com/example/catalogservicedevops/CatalogServiceDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogservicedevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceDevOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catalogservicedevops/CatalogServiceDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalogservicedevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
